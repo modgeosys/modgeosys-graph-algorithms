41ed19eb077547b2a27d130f532f7aafafe686ec
Kevin Weller <klweller@icloud.com>
2024-01-22 19:13:15 -0700

    Add a missing test for manhattan_distance().

--- tests/modgeosys/nav/test_distance.py
+++ tests/modgeosys/nav/test_distance.py
@@ -11,6 +11,11 @@ def test_manhattan_distance_between_points_on_same_axis_is_absolute_difference()
     point_b = (1, 5)
     assert manhattan_distance(point_a, point_b) == 3
 
+def test_manhattan_distance_between_points_on_different_axes_is_sum_of_absolute_differences():
+    point_a = (0, 0)
+    point_b = (1, 1)
+    assert manhattan_distance(point_a, point_b) == 2
+
 
 def test_euclidean_distance_between_identical_points_is_zero():
     point = (1, 2)
