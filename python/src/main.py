5b13447eb5ece5c64513e619439de430fcb7d92e
Kevin Weller <klweller@icloud.com>
2024-01-28 14:29:51 -0700

    Fix example and README.

--- python/src/main.py
+++ python/src/main.py
@@ -1,11 +1,11 @@
 """Usage example(s)."""
 
 from modgeosys.nav.a_star import a_star
-from modgeosys.nav.types import Edge, Graph
+from modgeosys.nav.types import Node, Edge, Graph
 from modgeosys.nav.distance import manhattan_distance, euclidean_distance
 
 # Define a graph.
-nodes = [(0.0, 0.0), (0.0, 2.0), (1.0, 0.0), (2.0, 1.0), (2.0, 3.0)]
+nodes = [Node((0.0, 0.0)), Node((0.0, 2.0)), Node((1.0, 0.0)), Node((2.0, 1.0)), Node((2.0, 3.0))]
 edges = (Edge(weight=2.0, node_indices=frozenset((0, 1))),
          Edge(weight=1.0, node_indices=frozenset((0, 2))),
          Edge(weight=1.0, node_indices=frozenset((2, 3))),
