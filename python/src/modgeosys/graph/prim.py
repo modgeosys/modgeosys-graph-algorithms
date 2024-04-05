e17c22e41ae24674e6d351876936b6d79663cc26
Kevin Weller <klweller@icloud.com>
2024-04-05 09:31:36 -0600

    Minor whitespace adjustments.

--- python/src/modgeosys/graph/prim.py
+++ python/src/modgeosys/graph/prim.py
@@ -38,7 +38,6 @@ def prim(graph: Graph, start_node_index: int, edge_is_valid: ValidEdgeCallable =
                     included_node_indices.add(new_node_index)
                 if new_node_index in excluded_node_indices:
                     excluded_node_indices.remove(new_node_index)
-
                 included_edges.add(best_edge)
                 excluded_edges.remove(best_edge)
 
@@ -53,7 +52,7 @@ def prim(graph: Graph, start_node_index: int, edge_is_valid: ValidEdgeCallable =
     return included_edges
 
 
-def find_first_edge_with_node_index(edges: list[tuple[Edge, int]], node_index_1: int, node_index_2: int = None) -> tuple[Edge, int]:
+def find_first_edge_with_node_index(edges: list[Edge], node_index_1: int, node_index_2: int = None) -> Edge or None:
     """Perform a sequential search for the first edge with the given node index."""
     for edge in edges:
         if node_index_1 in edge.node_indices or (node_index_2 and node_index_2 in edge.node_indices):
