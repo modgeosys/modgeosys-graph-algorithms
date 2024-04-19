eada66844e9e042022a6db55bca0e10d744746fa
Kevin Weller <klweller@icloud.com>
2024-04-19 17:11:00 -0600

    Checkpoint 4: Update example script (still fails along with tests until picked test graphs can be updated).

--- python/src/main.py
+++ python/src/main.py
@@ -14,17 +14,18 @@ toy_graph = Graph.from_edge_definitions(((2, ((0.0, 0.0), (0.0, 2.0))),
                                          (1, ((0.0, 0.0), (1.0, 0.0))),
                                          (1, ((1.0, 0.0), (2.0, 1.0))),
                                          (3, ((0.0, 2.0), (2.0, 3.0))),
-                                         (1, ((2.0, 1.0), (2.0, 3.0)))))
+                                         (1, ((2.0, 1.0), (2.0, 3.0)))),
+                                        properties={'heuristic_distance': manhattan_distance})
 
 # Load a bigger graph from a pickle file.
 with open('python/data/graph.pickle', 'rb') as pickled_sample_larger_graph_file:
     larger_graph = pickle.load(pickled_sample_larger_graph_file)
 
 # Call the A* function.
-toy_a_star_path = a_star(graph=toy_graph, start_node_index=0, goal_node_index=4, heuristic_distance=manhattan_distance)
+toy_a_star_path = a_star(graph=toy_graph, start_node_index=0, goal_node_index=4)
 print(f'Toy A* Path:')
 pprint(toy_a_star_path)
-larger_a_star_path = a_star(graph=larger_graph, start_node_index=0, goal_node_index=4, heuristic_distance=manhattan_distance)
+larger_a_star_path = a_star(graph=larger_graph, start_node_index=0, goal_node_index=4)
 print()
 print(f'Large A* Path:')
 pprint(larger_a_star_path)
